75a9afd22d822bb7f73eb89d8dfc8b4b4fd9bb0b
savina apriliani hermawan <savinaaprln@student.telkomuniversity.ac.id>
2024-04-05 10:17:54 +0700

    Membuat tampian utama

--- app/src/main/java/org/d3if3120/mobpro1assesment/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3120/mobpro1assesment/ui/screen/MainScreen.kt
@@ -1,21 +1,43 @@
 package org.d3if3120.mobpro1assesment.ui.screen
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -41,7 +63,7 @@ fun MainScreen(navController: NavHostController) {
                     IconButton(onClick = {
                         navController.navigate(Screen.About.route)
                     }) {
-                        Icon(imageVector = Icons.Filled.Favorite,
+                        Icon(imageVector = Icons.Filled.AccountCircle,
                             contentDescription = stringResource(id = R.string.tentang_aplikasi),
                             tint = MaterialTheme.colorScheme.primary
                         )
@@ -56,15 +78,83 @@ fun MainScreen(navController: NavHostController) {
 
 @Composable
 fun ScreenContent(modifier: Modifier ){
+
+    var celcius by rememberSaveable { mutableStateOf("") }
+    val radioOptions = listOf(
+        stringResource(id = R.string.kelvin),
+        stringResource(id = R.string.fharenheit)
+    )
+    var pilihanSuhu by rememberSaveable { mutableStateOf(radioOptions[0])}
+
     Column (
         modifier = modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .verticalScroll(rememberScrollState())
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ){
+    Text(text = stringResource(id = R.string.app_intro),
+        style = MaterialTheme.typography.bodyLarge,
+        modifier = Modifier.fillMaxWidth()
+    )
+        OutlinedTextField(
+            value =celcius ,
+            onValueChange ={celcius = it},
+            label = { Text(text = stringResource(id = R.string.celcius_label))},
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        //radioOption untuk pilihan kelvin atau farenheit
+        Row (
+            modifier = Modifier
+                .padding(top = 9.dp)
+                .border(1.dp, Color.Gray, RoundedCornerShape(4.dp))
+        ){
+            radioOptions.forEach { text ->
+                SuhuOption(
+                    label = text,
+                    isSelected = pilihanSuhu == text ,
+                    modifier = Modifier
+                        .selectable(
+                            selected = pilihanSuhu == text,
+                            onClick = { pilihanSuhu = text },
+                            role = Role.RadioButton
+                        )
+                        .weight(1f)
+                        .padding(16.dp)
+                )
+
+            }
+        }
+        Button(onClick = { /*TODO*/ },
+            modifier = Modifier.padding(top = 8.dp),
+            contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
+        ) {
+            Text(text = stringResource(id = R.string.hitung))
+        }
 
     }
 }
 
+@Composable
+fun SuhuOption(label: String, isSelected: Boolean, modifier: Modifier) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        RadioButton(selected = isSelected, onClick = null)
+        Text(
+            text = label,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(start = 8.dp)
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
